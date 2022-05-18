164da50b6852e563c07cd4397576c666a2c46c02
Reuben Gann <reubengann@gmail.com>
2022-05-17 23:41:07 -0400

    Add rough data structures

---
+++ WpfApp1/Slideshow.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+
+namespace WpfApp1
+{
+    public class Slideshow
+    {
+        List<Slide> Slides;
+        public Slideshow()
+        {
+            Slides = new List<Slide>();
+        }
+    }
+
+    class Slide
+    {
+        Color BackgroundColor;
+        List<SlideItem> Items;
+    }
+
+    class SlideItem
+    {
+
+    }
+
+    class SlideText : SlideItem
+    {
+        string Text;
+        Color color;
+    }
+
+    class SlideImage : SlideItem
+    {
+        string Path;
+
+    }
+}
