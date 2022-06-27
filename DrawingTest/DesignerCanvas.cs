1889fff0a841bb607c7b23e183cd8c2e69d7b5dc
Reuben Gann <reubengann@gmail.com>
2022-06-26 22:00:22 -0400

    Move to component model

---
+++ DrawingTest/DesignerCanvas.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace DrawingTest
+{
+    public class DesignerCanvas : Canvas
+    {
+
+        public DesignerCanvas()
+        {
+            DesignerItem item = new DesignerItem();
+            item.Content = new Ellipse() { Width = 100, Height = 100, Fill = new SolidColorBrush(Colors.Red), StrokeThickness = 2 };
+            this.Children.Add(item);
+        }
+    }
+}
---
+++ DrawingTest/DesignerItem.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace DrawingTest
+{
+    public class DesignerItem : ContentControl
+    {
+    }
+}
